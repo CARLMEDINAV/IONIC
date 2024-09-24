c29ce8d4a936e0f0cfdccaec9db6d6a7b16682ee
CARLMEDINAV <CARL.MEDINAV@DUOCUC.CL>
2024-09-24 09:32:06 -0300

    QR URL

--- src/app/pages/unirse-a/unirse-a.page.ts
+++ src/app/pages/unirse-a/unirse-a.page.ts
@@ -6,33 +6,11 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
   styleUrls: ['./unirse-a.page.scss'],
 })
 export class UnirseAPage implements OnInit {
-  @ViewChild('video', { static: false }) video!: ElementRef<HTMLVideoElement>;
-  @ViewChild('canvas', { static: false }) canvas!: ElementRef<HTMLCanvasElement>;
-  imageUrl: string | undefined;
+  
 
   constructor() {}
 
   ngOnInit() {
-    this.startCamera();
-  }
-
-  async startCamera() {
-    try {
-      const stream = await navigator.mediaDevices.getUserMedia({ video: true });
-      this.video.nativeElement.srcObject = stream;
-      this.video.nativeElement.play();
-    } catch (error) {
-      console.error('Error al acceder a la cámara:', error);
-    }
-  }
-
-  takePicture() {
-    const context = this.canvas.nativeElement.getContext('2d');
-    if (context) { // Verifica que el contexto no sea nulo
-      context.drawImage(this.video.nativeElement, 0, 0, this.canvas.nativeElement.width, this.canvas.nativeElement.height);
-      this.imageUrl = this.canvas.nativeElement.toDataURL('image/png');
-    } else {
-      console.error('No se pudo obtener el contexto del canvas');
-    }
+    
   }
 }
