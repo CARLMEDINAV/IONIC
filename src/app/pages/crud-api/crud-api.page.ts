f6b5134058122f45a42e1833f5d1e532a3f10daf
Jesus johnson <je.johnson@duocuc.cl>
2024-10-17 01:24:17 -0300

    Api con diseño

--- src/app/pages/crud-api/crud-api.page.ts
+++ src/app/pages/crud-api/crud-api.page.ts
@@ -11,20 +11,39 @@ import { CrudApiDjangoService } from 'src/app/servicio/crud-api-django.service';
 })
 export class CrudApiPage implements OnInit {
 
-  constructor(private api:CrudApiDjangoService ){ }
+  usuarios: any[] = [];     // Para almacenar los datos de usuarios
+  estudiantes: any[] = [];  // Para almacenar los datos de estudiantes
+  mostrarUsuarios: boolean = false;   // Controla la visibilidad de usuarios
+  mostrarEstudiantes: boolean = false; // Controla la visibilidad de estudiantes
+
+  constructor(private api: CrudApiDjangoService) { }
 
   ngOnInit() {
   }
 
-
-  recuperar(){
+  recuperar1() {
     this.api.getInformacion().subscribe(
-      (resp)=>{
-      console.log(resp[0])
-    },
-    (error)=>{
-      console.log(error)
-    }
-  )
+      (resp) => {
+        this.usuarios = resp;  
+        this.mostrarUsuarios = true; // Muestra la lista de usuarios
+        console.log(this.usuarios);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
+  }
+
+  recuperar2() {
+    this.api.getInformacion2().subscribe(
+      (resp) => {
+        this.estudiantes = resp;  
+        this.mostrarEstudiantes = true; // Muestra la lista de estudiantes
+        console.log(this.estudiantes);
+      },
+      (error) => {
+        console.log(error);
+      }
+    );
   }
 }
--- src/app/servicio/crud-api-django.service.ts
+++ src/app/servicio/crud-api-django.service.ts
@@ -6,13 +6,21 @@ import { Observable, retry } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
+
 export class CrudApiDjangoService {
 
-  constructor(private http: HttpClient ) { }
+  private rutaApiDjango = "http://127.0.0.1:8000/api/usuarios/";
+  private rutaApiOtra = "http://127.0.0.1:8000/api/estudiantes/"; // Cambia esto a la URL de tu otra API
 
-  rutaApi="http://127.0.0.1:8000/api/usuarios/"
-  
-  getInformacion():Observable<any>{
-    return this.http.get(this.rutaApi).pipe(retry(3))
+  constructor(private http: HttpClient) { }
+
+  // Método para obtener información de la primera API
+  getInformacion(): Observable<any> {
+    return this.http.get(this.rutaApiDjango).pipe(retry(3));
+  }
+
+  // Método para obtener información de la segunda API
+  getInformacion2(): Observable<any> {
+    return this.http.get(this.rutaApiOtra).pipe(retry(3));
   }
 }
